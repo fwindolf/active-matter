af7173e1d51e4e80cc7cfd415206a6d16ae2c202
Florian Windolf <windolf1@aalto.fi>
2018-08-23 10:05:05 +0300

    Updated to use local storage for data and additional params.

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=48:00:00      # 2d
-#SBATCH --mem=16G  	     # 16G of memory
+#SBATCH --mem=16G  	         # 16G of memory
 #SBATCH --gres=gpu
 #SBATCH --constraint=pascal
 
@@ -14,35 +14,57 @@ which python
 
 conda list | grep -E "keras|tensorflow|opencv"
 
+# Copy data file to temp disk and unpack
+mkdir /tmp/$SLURM_JOB_ID
+trap "rm -rf /tmp/$SLURM_JOB_ID; exit" TERM EXIT # Set trap for unscheduled exit
+
+cp $WRKDIR/data.tar /tmp/$SLURM_JOB_ID
+cd /tmp/$SLURM_JOB_ID
+tar xf data.tar 
+
+# Go to work dir and start training
 cd $WRKDIR/am2018
 
 # Parameters for training
-m=seg_net
-dp="$WRKDIR/data/2700/tau_20_var_150 $WRKDIR/data/2700/tau_20_var_200  $WRKDIR/data/2700/tau_50_var_120 $WRKDIR/data/2700/tau_50_var_150 $WRKDIR/data/1200/tau_20_var_120 $WRKDIR/data/1200/tau_20_var_150 $WRKDIR/data/1200/tau_20_var_180 $WRKDIR/data/1200/tau_50_var_100  $WRKDIR/data/1200/tau_50_var_120 $WRKDIR/data/1200/tau_50_var_150"
-s=stacked
-dh=480
-dw=480
-dz=3
-ds=0.4
+datadir=/tmp/$SLURM_JOB_ID
 
-tc=1
+# Parameters for training
+m=lstm_shallow
+dp="$datadir/data/2700/tau_20_var_150 $datadir/data/2700/tau_20_var_200 $datadir/data/2700/tau_50_var_120 $datadir/data/2700/tau_50_var_150 $datadir/data/1200/tau_20_var_120 $datadir/data/1200/tau_20_var_180 $datadir/data/1200/tau_50_var_100 $datadir/data/1200/tau_50_var_150"
+l=true
+s=sequence
+dh=448
+dw=448
+dn=1
+dz=4
+da=0.8
 tr=0.1
-tb=8
-te=200
-ts=0.15
+tb=16
+te=1
+ts=0.1
+tc=0
+tl=dice
 
 echo "--------------------------------------------------------"
 echo "PARAMETERS"
 echo "MODEL     : $m"
+echo "LABELED   : $l"
 echo "DATA_PATHS: $dp"
 echo "STRUCTURE : $s"
-echo "INPUT_DIMS: ($dh, $dw, $dz)"
-echo "CROPS     : $tc with pre-scale of $ds"
+echo "INPUT_DIMS: ($dh, $dw, $dn) with stacksize $dz"
 echo "LR        : $tr"
 echo "BATCHSIZE : $tb"
 echo "EPOCHS    : $te"
 echo "SPLIT     : $ts"
+echo "CROPS     : $tc with area $da"
+echo "LOSS      : $tl"
 echo "--------------------------------------------------------"
 
 # Start training
-srun python train.py -m $m -dp $dp -l -s $s -dh $dh -dw $dw -dz $dz -ds $ds -tr $tr -tb $tb -te $te -ts $ts -tc $tc
+if [ "$l" = true ] ; then
+    python train.py -m $m -l -dp $dp -s $s -dh $dh -dw $dw -dn $dn -dz $dz -da $da -tr $tr -tb $tb -te $te -ts $ts -tc $tc -tl $tl
+else
+    python train.py -m $m -dp $dp -s $s -dh $dh -dw $dw -dn $dn -dz $dz -da $da -tr $tr -tb $tb -te $te -ts $ts -tc $tc -tl $tl
+fi
+
+mv /tmp/$SLURM_JOB_ID/output $WRKDIR/am2018
